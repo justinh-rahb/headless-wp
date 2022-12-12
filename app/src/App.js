ce171418d5e479bfedf0b3757fad9fdfd6910a52
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2022-12-12 00:09:00 -0500

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -2,32 +2,23 @@ import React, { useState, useEffect } from "react";
 import WPAPI from "wpapi";
 
 const App = () => {
- // Initialize the state for the products and the current page
+ // Initialize the state for the products
  const [products, setProducts] = useState([]);
- const [page, setPage] = useState(1);
 
- // Make the App component's state and functions available globally
- window.App = {
-   state: {
-     page: page
-   },
-   setPage: setPage
- };
-
- // Fetch the products when the page changes
+ // Fetch the products when the component is mounted
  useEffect(() => {
 	// Initialize the WPAPI client
 	const wc = new WPAPI({
- 	endpoint: "https://store.rahb.ca/wp-json/wc/v3",
+ 	endpoint: "https://my-store.com/wp-json/wc/v3",
  	consumerKey: process.env.WC_CONSUMER_KEY,
  	consumerSecret: process.env.WC_CONSUMER_SECRET
 	});
 
-	// Get the products for the current page
-	wc.products().page(page).then(products => {
+	// Get the products for the first page
+	wc.products().per_page(12).page(1).then(products => {
  	setProducts(products);
 	});
- }, [page]);
+ }, []);
 
  // Render the products on the page
  return (
